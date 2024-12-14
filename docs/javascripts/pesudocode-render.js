f8aadc6bec89317862f0f446126d8dea925ea8b3
YufengPeng <yufengpeng@YufengPengdeMacBook-Air.local>
2024-12-14 15:13:15 +0800

    Change DOMAIN and workflow

--- docs/javascripts/pesudocode-render.js
+++ docs/javascripts/pesudocode-render.js
@@ -1,6 +1,5 @@
-let pseudocodes = document.querySelectorAll(".pseudocode");
-
-var DEFAULT_OPTIONS = {
+// 定义默认选项
+const DEFAULT_OPTIONS = {
     indentSize: '1.2em',
     commentDelimiter: '//',
     lineNumber: true,
@@ -10,8 +9,21 @@ var DEFAULT_OPTIONS = {
     scopeLines: true
 };
 
-pseudocodes.forEach((pse) => {
-    let code = pse.textContent;
-    pse.innerHTML = '';
-    pseudocode.render(code, pse, DEFAULT_OPTIONS);
+// 渲染伪代码函数
+function renderPseudocode() {
+    // 选择所有具有.pseudocode类的元素
+    let pseudocodes = document.querySelectorAll(".pseudocode");
+
+    pseudocodes.forEach((pse) => {
+        // 获取原始文本并清空HTML内容以防重复渲染
+        let code = pse.textContent;
+        pse.innerHTML = '';
+        // 使用pseudocode.js库进行渲染
+        pseudocode.render(code, pse, DEFAULT_OPTIONS);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // 页面首次加载时渲染伪代码
+    renderPseudocode();
 });
